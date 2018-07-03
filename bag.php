4af6a7cf18f8a019271a3db9ce1eeced3abb6bfb
松下望 <matsu.uner@gmail.com>
2018-07-03 20:32:07 +0900

    bagページ ベースコーディング

--- bag.php
+++ bag.php
@@ -1,3 +1,66 @@
 <?php include_once(dirname(__FILE__) . '/elements/head.php'); ?>
 <?php include_once(dirname(__FILE__) . '/elements/header.php'); ?>
+
+<div class="container">
+    <h2 class="bb serif text-center py-3">Item Collection</h2>
+    <section class="mt-5 mx-5">
+        <h3>●特徴</h3>
+        <p>2018SSモデルのXフレームラインとアーカイブラインは1980年代時モデルをアップデート。</p>
+    </section>
+    <section class="mt-5 mx-5">
+        <h4>＜Xフレームライン＞</h4>
+        <p>素材：1680Dポリエステル　カラー：BLACK/NAVY</p>
+        <p>
+            背景にX字型のアルミフレーム = Xフレームを採用。これは1980年代モデルにも使用されていました。
+            これにより強度と軽さを両立し、背景のメッシュ生地との間のエアチャンネルによりクッション性と快適性も実現しています。
+        </p>
+        <div class="row text-center mt-5">
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+        </div>
+    </section>
+    <section class="mt-5 mx-5">
+        <h4>＜アーカイブライン＞</h4>
+        <p>素材：1000Dコーデュラナイロン　カラー：BLACK/NAVY/WINE/BEIGE</p>
+        <p>
+            アウトドアだけでなく都会でのカジュアルユースにも馴染むデザイン。
+        </p>
+        <div class="row text-center mt-5">
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+            <div class="col-md-3">
+                <img src="../assets/images/top_bag.jpg" alt="">
+                <div>X-F MOUSETRAP</div>
+            </div>
+        </div>
+    </section>
+    <div class="text-right mt-5 pb-3 color-dark bb">
+        キタムラトレードサービス <a href="tel:03-6214-0104">TEL 03-6214-0104</a>
+    </div>
+</div>
+
 <?php include_once(dirname(__FILE__) . '/elements/footer.php'); ?>
