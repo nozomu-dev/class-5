fe26deed5663c689a00e942801efaa5afacdd6ed
松下望 <matsu.uner@gmail.com>
2018-07-03 20:57:52 +0900

    アパレルページ - 画像差し込み

--- apparel.php
+++ apparel.php
@@ -6,46 +6,46 @@
 
     <div class="row text-center mt-5">
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-st-shaggy.jpg" alt="">
+            <div>Shaggy Ox Bd Shirt</div>
         </div>
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-st-heavy.jpg" alt="">
+            <div>Super Heavy Check Shirt</div>
         </div>
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
-        </div>
-    </div>
-
-    <div class="row text-center mt-5">
-        <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
-        </div>
-        <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
-        </div>
-        <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-st-flannel.jpg" alt="">
+            <div>Check Flannel Shirt</div>
         </div>
     </div>
 
     <div class="row text-center mt-5">
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-jk-engineer.jpg" alt="">
+            <div>Engineer Jacket</div>
         </div>
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-jk-railroad.jpg" alt="">
+            <div>Railroad Jacket</div>
         </div>
         <div class="col-md-4">
-            <img src="../assets/images/top_tops.jpg" alt="">
-            <div>X-F MOUSETRAP</div>
+            <img class="mw-100" src="../assets/images/apparel-jk-warden.jpg" alt="">
+            <div>Warden Jacket</div>
+        </div>
+    </div>
+
+    <div class="row text-center mt-5">
+        <div class="col-md-4">
+            <img class="mw-100" src="../assets/images/apparel-pt-duck.jpg" alt="">
+            <div>Duck Painter Pants2</div>
+        </div>
+        <div class="col-md-4">
+            <img class="mw-100" src="../assets/images/apparel-pt-explorer.jpg" alt="">
+            <div>Explorer Shorts</div>
+        </div>
+        <div class="col-md-4">
+            <img class="mw-100" src="../assets/images/apparel-pt-husky.jpg" alt="">
+            <div>Husky Half Pants</div>
         </div>
     </div>
 
