d4bc32579051e35d1459bdc4d6913bbfb8cbe576
松下望 <matsu.uner@gmail.com>
2018-07-03 20:45:32 +0900

    アパレルページ ベースコーディング

--- apparel.php
+++ apparel.php
@@ -1,3 +1,57 @@
 <?php include_once(dirname(__FILE__) . '/elements/head.php'); ?>
 <?php include_once(dirname(__FILE__) . '/elements/header.php'); ?>
+
+<div class="container">
+    <h2 class="bb serif text-center py-3">Item Collection</h2>
+
+    <div class="row text-center mt-5">
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+    </div>
+
+    <div class="row text-center mt-5">
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+    </div>
+
+    <div class="row text-center mt-5">
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+        <div class="col-md-4">
+            <img src="../assets/images/top_tops.jpg" alt="">
+            <div>X-F MOUSETRAP</div>
+        </div>
+    </div>
+
+    <div class="text-right mt-5 pb-3 color-dark bb">
+        シープ <a href="tel:03-3833-1641">TEL 03-3833-1641</a>
+    </div>
+</div>
+
 <?php include_once(dirname(__FILE__) . '/elements/footer.php'); ?>
