b20eb498ceb82d0ed302bacd0dd9ac1bc2e368c2
松下望 <matsu.uner@gmail.com>
2018-07-10 20:58:16 +0900

    copyrightを変更

--- elements/footer.php
+++ elements/footer.php
@@ -1,6 +1,6 @@
 
     <div class="container text-center mt-5 pb-5">
-        <div class="copyright">&copy; class-5</div>
+        <div class="copyright">&copy; class5 company</div>
     </div>
 
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
